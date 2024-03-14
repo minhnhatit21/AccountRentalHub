22c93cffe9b48d76700e9f08cd655faf769ac366
minhnhatit21 <nhmnhat2101.it@gmail.com>
2024-03-14 16:53:17 +0700

    Initial Setup

---
+++ account_rental_hub-backend/src/main/java/com/AccountRentalHub/AccountRentalHubApplication.java
@@ -0,0 +1,13 @@
+package com.AccountRentalHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountRentalHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountRentalHubApplication.class, args);
+	}
+
+}
---
+++ account_rental_hub-backend/src/test/java/com/AccountRentalHub/AccountRentalHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AccountRentalHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountRentalHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
