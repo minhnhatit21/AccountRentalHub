d4249d661add7d2fedda4e21b7a6863ff895fea3
minhnhatit21 <nhmnhat2101.it@gmail.com>
2024-05-03 21:04:24 +0700

    Implement login page, homepage

---
+++ account_rental_hub-backend/src/main/java/com/AccountRentalHub/payload/request/ForgotPasswordRequest.java
@@ -0,0 +1,2 @@
+package com.AccountRentalHub.payload.request;public class ForgotPasswordRequest {
+}
---
+++ account_rental_hub-backend/src/main/java/com/AccountRentalHub/security/services/ResetTokenService.java
@@ -0,0 +1,2 @@
+package com.AccountRentalHub.security.services;public interface ResetTokenService {
+}
